5dd9bcc5e8afe349ab8fcec7e62d524155ab0d1f
Letherius Miller <lmiller@alabamashipyard.com>
2022-10-05 13:36:43 -0500

    first homework ass.

--- register.js
+++ register.js
@@ -1,41 +1,37 @@
-//object literal {}
-let petSalon = {
-//atributes
-    name:"The Fashion Pet",
-    phone:"777-777-7777",
-    address:{
-        country:"Mex",
-        city:"Tijuana",
-        street: "Universidad",
-        number:"263-k",
-        zip:"22141"
-    },
-    pets:[]//instead of students use pets
+
+
+let pets = [
+    {
+    name:"Steel",
+    age:5,
+    gender:"Male",
+    service:"Bath"
+},
+
+{
+    name:"Hulk",
+    age:3,
+    gender:"Male",
+    service:"Teeth Cleaning"
+},
+
+{
+    name:"Lucky",
+    age:10,
+    gender:"Male",
+    service:"Nail Trimming"
 }
 
-let students = [
-    {
-        name:"Myles",
-        age:98,
-        grade:"A"
-},
-{
-    name:"Tuong",
-    age:98,
-    grade:"A-"
-},
-{
-    name:"Miguel",
-    age:97,
-    grade:"F"
-}
 ];
 //FOR LOOP: start point; stopping condition; increment
 //length return the number of elements in the array
-for(let i=0; i<students.length; i++) {
-console.log(students[i].name);
+for(let i=0; i<pets.length; i++) {
+console.log(pets[i].name);
+console.log(pets[i].age);
+console.log(pets[i].gender);
+console.log(pets[i].service);
 }
-console.log("We have " + students.length + " students");//--use alert instead of console
+
 
 
 //brute force solution
