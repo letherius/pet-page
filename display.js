62e4198301a00687ffec6a0346051b200d605229
Letherius Miller <lmiller@alabamashipyard.com>
2022-10-12 16:02:35 -0500

    almost there

--- display.js
+++ display.js
@@ -17,6 +17,20 @@ tmp+=`
 petsDiv.innerHTML = tmp;
 }
 
-function displayPetTable(){
-
+function displaypetTable(){
+let petTable = document.getElementById("petTable");
+let tmp="";
+for(let i=0; i<petSalon.pets.length;i++){
+let aPet = petSalon.pets[i];
+tmp+=`
+    <table id="${aPet.id}" class="table table-bordered">
+        <p>Name: ${aPet.name} </p>
+        <p>Age: ${aPet.age} </p>
+        <p>Gender: ${aPet.gender}</p>
+        <p>Breed: ${aPet.breed}</p>
+        <p>Service: ${aPet.service}</p>
+        <button class="btn btn-danger" onclick="deletePet(${aPet.id});">Delete</button>
+    </div>
+    `;
+}
 }
