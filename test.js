b3cc2dc2c71ee62c903a941fb784851f2eb672f1
Letherius Miller <letherius1@gmail.com>
2024-04-17 10:39:48 -0500

    changed to home to index.html

--- test.js
+++ test.js
@@ -1,29 +1,3 @@
-// let i = 99;
-// while (i>0) {
-//     console.log(i + " bottles of beer on the wall");
-//     i -= 1;
-//     if (i==1 && i==1){
-//         break;
-//     } else if (condition){
-
-//     } else {
-//         return console.log("not a number")
-//     }
-// }
-
-//given an integer n, return a string array
-
-// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-// answer[i] == "Fizz" if i is divisible by 3.
-// answer[i] == "Buzz" if i is divisible by 5.
-// i = 3
-// i=5
-// i % 3 == 0
-// i % 5 == 0
-
-// 0 == true
-// 1 == false
-
 
 function fizzbuzz (n){
 
